3a1d26497710d3b22a5ba95ddf868842903bbdc9
Eslam Aly <eslam.mahmud18@gmail.com>
2025-10-06 19:30:02 +0200

    Completed the signup page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="bg-blue-500 text-white  w-full  shadow-md h-20 flex items-center sticky top-0 z-50">
+    <header className="bg-blue-800 text-white  w-full  shadow-md h-20 flex items-center sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between w-[90%] ">
         <div>
           <Link to="/" className="text-sm md:text-2xl font-bold">
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,84 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
-  return <div>Sign Up Page</div>;
-}
+  const [eroor, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [formData, setFormData] = React.useState({});
+  const navigate = useNavigate();
+  const handleChange = async (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-16 p-6  rounded-lg shadow-lg">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5 ">
+        <input
+          onChange={handleChange}
+          type="text"
+          id="username"
+          placeholder="username"
+          className="border p-3 rounded-lg "
+        />
+        <input
+          onChange={handleChange}
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border p-3 rounded-lg "
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-blue-800 text-white p-3 rounded-lg font-semibold"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/signin" className="text-blue-800 font-semibold">
+          {" "}
+          Login
+        </Link>
+      </div>
+      {eroor && <p className="text-red-600 mt-3">{eroor}</p>}
+    </div>
+  );
+}
 export default SignUp;
