c0841e8ea1f9213a686000e07f90b2e7c0dc064b
Eslam Aly <eslam.mahmud18@gmail.com>
2025-10-30 19:13:38 +0100

    deleted old createListing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SignUp from "./pages/SignUp.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
-import CreateListing from "./pages/CreateListing.jsx";
+
 import UpdateListing from "./pages/UpdateListing.jsx";
 import Listing from "./pages/Listing.jsx";
 import Search from "./pages/Search.jsx";
@@ -37,7 +37,6 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
           <Route
             path="/update-listing/:listingId"
             element={<UpdateListing />}
